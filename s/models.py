15bd9b674b48ebbd09cdf8766d143aaf78320354
clyde <oodennis20@gmail.com>
2020-01-17 16:15:50 +0300

    initial migration

--- s/models.py
+++ s/models.py
@@ -6,10 +6,28 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
 
+class Hood(models.Model):
+    hood_photo = ImageField(blank=True,manual_crop='')
+    hood_name = models.CharField(max_length=100, blank=True, null=True)
+    occupants_count = models.IntegerField(blank=True, null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Business(models.Model):
+    b_photo = ImageField(blank=True,manual_crop='')
+    b_name = models.CharField(max_length=100, blank=True, null=True)
+    b_description = models.TextField(max_length=200, blank=True, null=True)
+    b_email = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     profile_photo = ImageField(blank=True,manual_crop='')
-    
+    bio= models.CharField(max_length=240, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    u_hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
+
     @receiver(post_save, sender=User)
     def create_profile(sender,instance,created,**kwargs):
         if created:
@@ -28,17 +46,4 @@ class Profile(models.Model):
 
         return profile
 
-class Hood(models.Model):
-    hood_photo = ImageField(blank=True,manual_crop='')
-    hood_name = models.CharField(max_length=100, blank=True, null=True)
-    occupants_count = models.IntegerField(blank=True, null=True)
-    admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
-
-class Business(models.Model):
-    b_photo = ImageField(blank=True,manual_crop='')
-    b_name = models.CharField(max_length=100, blank=True, null=True)
-    b_description = models.TextField(max_length=200, blank=True, null=True)
-    b_email = models.CharField(max_length=100, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
