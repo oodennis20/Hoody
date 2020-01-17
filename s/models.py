9b77df04a30d54e9ffa3866f49163f7f61697c65
clyde <oodennis20@gmail.com>
2020-01-17 15:28:17 +0300

    added signals to generate automatic profiles for new users

--- s/models.py
+++ s/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.db import models
+from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = ImageField(blank=True,manual_crop='')
+    
+    @receiver(post_save, sender=User)
+    def create_profile(sender,instance,created,**kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender,instance, **kwargs):
+        instance.profile.save()
+    
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+
+        return profile
--- s/views.py
+++ s/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            current_user = form.save(commit=False)
+            current_user.is_active = False
+            current_user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your Hoods account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': current_user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(current_user.pk)),
+                'token':account_activation_token.make_token(current_user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        current_user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        current_user = None
+    if current_user is not None and account_activation_token.check_token(current_user, token):
+        current_user.is_active = True
+        current_user.save()
+        login(request, current_user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. <a href="https://Hoodies.herokuapp.com"> Login </a> Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+@login_required(login_url='/accounts/login')
+def profile(request,profile_id):
+
+    profile = Profile.objects.get(pk = profile_id)
+
+    return render(request,"profile.html",{"profile":profile})
