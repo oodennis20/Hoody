1bea9e0be0696b83d6981582a2dc09c1e9602089
clyde <oodennis20@gmail.com>
2020-01-18 14:52:06 +0300

    posts displayed

--- s/views.py
+++ s/views.py
@@ -58,7 +58,7 @@ def home(request):
     if request.user.is_authenticated:
         if Join.objects.filter(user_id = request.user).exists():
             hood = Hood.objects.get(pk = request.user.join.hood_id.id)
-            posts =" Posts.objects.filter(hood = request.user.join.hood_id.id)"
+            posts =Posts.objects.filter(hood = request.user.join.hood_id.id)
             businesses = Business.objects.filter(hood = request.user.join.hood_id.id)
             return render(request,'hoods/hood.html',{"hood":hood,"businesses":businesses,"posts":posts})
         else:
