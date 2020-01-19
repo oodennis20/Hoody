471b5553de99aaf8bd2d4bcc8f4e5536fe79f18c
clyde <oodennis20@gmail.com>
2020-01-20 01:41:01 +0300

    made changes to make login direct

--- s/urls.py
+++ s/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     url(r'^$', views.home,name='home'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^logout/$',views.logout_request,name="logout"),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
     url(r'^join/(\d+)',views.join,name = 'join'),
     url(r'^profile/$',views.profile,name = 'profile'),
     url(r'^editprofile/$',views.edit_profile,name= 'edit_profile'),
--- s/views.py
+++ s/views.py
@@ -15,45 +15,18 @@ from django.core.mail import EmailMessage
 from django.contrib import messages
 
 # Create your views here.
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            current_user = form.save(commit=False)
-            current_user.is_active = False
-            current_user.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your Hoody account.'
-            message = render_to_string('registration/acc_active_email.html', {
-                'user': current_user,
-                'domain': current_site.domain,
-                'uid':urlsafe_base64_encode(force_bytes(current_user.pk)),
-                'token':account_activation_token.make_token(current_user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                        mail_subject, message, to=[to_email]
-            )
-            email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
+            user = form.save(commit=False)
+            user.save()
+
+        return redirect('home') 
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        current_user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        current_user = None
-    if current_user is not None and account_activation_token.check_token(current_user, token):
-        current_user.is_active = True
-        current_user.save()
-        login(request, current_user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. <a href="https://hoodies.herokuapp.com"> Login </a> Now you can login your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
 
 def home(request):
     if request.user.is_authenticated:
@@ -192,7 +165,7 @@ def delete_post(request,postId):
 	messages.error(request,'Succesfully Deleted a Post')
 	return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-@login_required(login_url='/account/login/')
+@login_required(login_url='/accounts/login/')
 def create_hood(request):
     current_user = request.user
     if request.method == 'POST':
